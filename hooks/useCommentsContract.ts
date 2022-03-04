318eb4bd176b3803d249afbaf3796f5858f6b808
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-03-04 18:12:27 -0500

    deployed on mumbai

--- hooks/useCommentsContract.ts
+++ hooks/useCommentsContract.ts
@@ -30,7 +30,7 @@ const useCommentsContract = () => {
   // We also pass in the signer if there is a signed in wallet, or if there's
   // no signed in wallet then we'll pass in the connected provider.
   const contract = wagmi.useContract({
-    addressOrName: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    addressOrName: "0xA7e41D801d0555A597fd6da22a395156e2A59e64",
     contractInterface: CommentsContract.abi,
     signerOrProvider: signer.data || provider,
   });
